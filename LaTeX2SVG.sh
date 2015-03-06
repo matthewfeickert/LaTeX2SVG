daf270ddd4151fd901e7594b13e5084201066e55
Matthew Feickert <mfeickert@gmail.com>
2015-03-06 00:18:55 -0600

    First commit and first push of working code.

---
+++ LaTeX2SVG.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Author: Matthew Feickert
+# Date: 2015-03-05
+# Description: This script takes a LaTeX file and will extract
+# the text or image in it, and export it to a SVG.
+# NOTE: The LaTeX document MUST use \pagestyle{empty} to avoid
+#       a column of whitespace trying to include the page number.
+
+# Check to make sure a file was passed
+if [ $# -eq 0 ]
+then
+  echo "LaTeX file name needed as input."
+  exit 1
+fi
+# Check to make sure a LaTeX file was passed
+if [ "${1:(-4)}" != ".tex" ]
+then
+  echo "Need to pass a .tex file."
+  exit 1
+fi
+# LaTeX -> SVG Script
+if [ "$2" != "verbose" ]        # If verbose option then output to stdout
+then
+  pdflatex $1 >/dev/null                  # pdflatex the input
+  pdfname=${1%.tex}.pdf                   # create variable name ending in .pdf
+  pdfcrop --clip $pdfname $pdfname >/dev/null # crop off all whitespace on pdf
+  pdf2svg $pdfname ${1%.tex}.svg >/dev/null   # convert to svg
+else
+  pdflatex $1
+  pdfname=${1%.tex}.pdf
+  pdfcrop --clip $pdfname $pdfname
+  pdf2svg $pdfname ${1%.tex}.svg
+fi
+# Cleanup
+rm -f *.aux *.log *.out
+rm -f $pdfname
